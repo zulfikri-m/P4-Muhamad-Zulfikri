0ea447392da02351bc9b67be398e311a528e0650
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:41:07 +0800

    Add files via upload
    Menampilkan array satu dimensi dengan bahasa C

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+	{
+		int myarray[14] = {1,23,17,4,-5,100,3,7,14,5,15,21,23,27};
+		printf("Nama : Muhamad Zulfikri \n");
+		printf("NIM : F1B019094 \n");
+		printf("Kelompok : 19 \n\n");
+		
+		printf("Data ke-1 =  %d \n",myarray[0]);
+		printf("Data ke-2 =  %d \n",myarray[1]);
+		printf("Data ke-3 =  %d \n",myarray[2]);
+		printf("Data ke-4 =  %d \n",myarray[3]);
+		printf("Data ke-5 =  %d \n",myarray[4]);
+		printf("Data ke-6 =  %d \n",myarray[5]);
+		printf("Data ke-7 =  %d \n",myarray[6]);
+		printf("Data ke-8 =  %d \n",myarray[7]);
+		printf("Data ke-9 =  %d \n",myarray[8]);
+		printf("Data ke-10 =  %d \n",myarray[9]);
+		printf("Data ke-11 =  %d \n",myarray[10]);
+		printf("Data ke-12 =  %d \n",myarray[11]);
+		printf("Data ke-13 =  %d \n",myarray[12]);
+		printf("Data ke-14 =  %d",myarray[13]);
+	}
