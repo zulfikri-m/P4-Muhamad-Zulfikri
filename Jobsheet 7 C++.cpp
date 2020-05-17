d6532c45abc2bd116cb1ec9b09b9bcdde461343c
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:54:38 +0800

    Add files via upload
    Mengisi elemen array menggunakan perulangan bahasa C++

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+	{
+		cout<<"Nama : Muhamad Zulfikri \n";
+		cout<<"NIM : F1B019094 \n";
+		cout<<"Kelompok : 19 \n\n";
+		
+		int x[4], i;
+		for(i=0; i<=4;i++)
+			{
+				cout<<"Masukkan nilai "<<i+1<<" : ";
+				cin>>x[i];
+			}
+		return 0;
+	}
