39d5f3fb79689ea6b136be26e6921dac86e74f17
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:49:55 +0800

    Add files via upload
    Menampilkan array satu dimensi dengan bahasa C++

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main()
+	{
+		int myarray[14] = {1,23,17,4,-5,100,3,7,14,5,15,21,23,27};
+		cout<<"Nama : Muhamad Zulfikri \n";
+		cout<<"NIM : F1B019094 \n";
+		cout<<"Kelompok : 19 \n\n";
+		
+		cout<<"Data ke-1 =  "<<myarray[0]<<endl;
+		cout<<"Data ke-2 =  "<<myarray[1]<<endl;
+		cout<<"Data ke-3 =  "<<myarray[2]<<endl;
+		cout<<"Data ke-4 =  "<<myarray[3]<<endl;
+		cout<<"Data ke-5 =  "<<myarray[4]<<endl;
+		cout<<"Data ke-6 =  "<<myarray[5]<<endl;
+		cout<<"Data ke-7 =  "<<myarray[6]<<endl;
+		cout<<"Data ke-8 =  "<<myarray[7]<<endl;
+		cout<<"Data ke-9 =  "<<myarray[8]<<endl;
+		cout<<"Data ke-10 = "<<myarray[9]<<endl;
+		cout<<"Data ke-11 = "<<myarray[10]<<endl;
+		cout<<"Data ke-12 = "<<myarray[11]<<endl;
+		cout<<"Data ke-13 = "<<myarray[12]<<endl;
+		cout<<"Data ke-14 = "<<myarray[13];
+	}
