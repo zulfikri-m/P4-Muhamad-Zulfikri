fbfdac2d730d721118b55370e3f6d6ad05e2b5b7
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:50:57 +0800

    Add files via upload
    Mengisi dan menampilkan elemen array secara dinamis dengan bahasa C++

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+	{
+		int x [14];
+		cout<<"Nama : Muhamad Zulfikri \n";
+		cout<<"NIM : F1B019094 \n";
+		cout<<"Kelompok : 19 \n\n";
+		
+		cout<<"Masukkan nilai 1 : ";
+		cin>>x[0];
+		cout<<"Masukkan nilai 2 : ";
+		cin>>x[1];
+		cout<<"Masukkan nilai 3 : ";
+		cin>>x[2];
+		cout<<"Masukkan nilai 4 : ";
+		cin>>x[3];
+		cout<<"Masukkan nilai 5 : ";
+		cin>>x[4];
+		cout<<"Masukkan nilai 6 : ";
+		cin>>x[5];
+		cout<<"Masukkan nilai 7 : ";
+		cin>>x[6];
+		cout<<"Masukkan nilai 8 : ";
+		cin>>x[7];
+		cout<<"Masukkan nilai 9 : ";
+		cin>>x[8];
+		cout<<"Masukkan nilai 10 : ";
+		cin>>x[9];
+		cout<<"Masukkan nilai 11 : ";
+		cin>>x[10];
+		cout<<"Masukkan nilai 12 : ";
+		cin>>x[11];
+		cout<<"Masukkan nilai 13 : ";
+		cin>>x[12];
+		cout<<"Masukkan nilai 14 : ";
+		cin>>x[13];
+	}
