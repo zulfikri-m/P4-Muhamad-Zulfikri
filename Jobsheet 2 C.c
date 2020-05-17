cae4100140ba78a85a5bcd760cc08ce923a95088
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:42:33 +0800

    Add files via upload
    Mengisi dan menampilkan elemen array secara dinamis dengan bahasa C

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+	{
+		int x [14];
+		printf("Nama : Muhamad Zulfikri \n");
+		printf("NIM : F1B019094 \n");
+		printf("Kelompok : 19 \n\n");
+		
+		printf("Masukkan nilai 1 : ");
+		scanf("%i",&x[0]);
+		printf("Masukkan nilai 2 : ");
+		scanf("%i",&x[1]);
+		printf("Masukkan nilai 3 : ");
+		scanf("%i",&x[2]);
+		printf("Masukkan nilai 4 : ");
+		scanf("%i",&x[3]);
+		printf("Masukkan nilai 5 : ");
+		scanf("%i",&x[4]);
+		printf("Masukkan nilai 6 : ");
+		scanf("%i",&x[5]);
+		printf("Masukkan nilai 7 : ");
+		scanf("%i",&x[6]);
+		printf("Masukkan nilai 8 : ");
+		scanf("%i",&x[7]);
+		printf("Masukkan nilai 9 : ");
+		scanf("%i",&x[8]);
+		printf("Masukkan nilai 10 : ");
+		scanf("%i",&x[9]);
+		printf("Masukkan nilai 11 : ");
+		scanf("%i",&x[10]);
+		printf("Masukkan nilai 12 : ");
+		scanf("%i",&x[11]);
+		printf("Masukkan nilai 13 : ");
+		scanf("%i",&x[12]);
+		printf("Masukkan nilai 14 : ");
+		scanf("%i",&x[13]);
+	}
