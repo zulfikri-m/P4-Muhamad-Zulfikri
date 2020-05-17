4ff20c025632d1027d8ca24c8ff3202910487ebf
zulfikri-m <64633675+zulfikri-m@users.noreply.github.com>
2020-05-17 12:55:26 +0800

    Add files via upload
    Mengisi dan menampilkan elemen array menggunakan perulangan bahasa C++

---
+++ Jobsheet 8 C++.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+	{
+		cout<<"Nama : Muhamad Zulfikri \n";
+		cout<<"NIM : F1B019094 \n";
+		cout<<"Kelompok : 19 \n\n";
+		
+		int x[4],i;
+		for (i=0; i<=4; i++)
+			{
+				cout<<"Masukkan nilai "<<i+1<<" : ";
+				cin>>x[i];
+			}
+		cout<<endl;
+		for (i=0; i<=4; i++)
+			{
+				cout<<"Nilai ke-"<<i+1<<" = "<<x[i]<<endl;
+			}
+		cout<<endl;
+		return 0;
+	}
